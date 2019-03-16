8cffb814991f23bc92493eda3088e50e89067d9b
Pejman Ghorbanzade <pejman@ghorbanzade.com>
2019-03-16 01:42:14 -0500

    updated license header block in all files

--- src/main/java/hw03/BackwardPrimes.java
+++ src/main/java/hw03/BackwardPrimes.java
@@ -1,11 +1,8 @@
 /**
-* CS114: Introduction to Java
-* Copyleft 2015 Pejman Ghorbanzade <mail@ghorbanzade.com>
-* More info: https://github.com/ghorbanzade/UMB-CS114-2015F
-*
-* The author has placed this file in the public domain.
-* He makes no warranty and accepts no liability for this file.
-*/
+ * UMB-CS114-2015F: Introduction to Programming in Java
+ * Copyright 2015 Pejman Ghorbanzade <pejman@ghorbanzade.com>
+ * More info: https://github.com/ghorbanzade/UMB-CS114-2015F
+ */
 
 package edu.umb.cs114.hw03.q5;
 
